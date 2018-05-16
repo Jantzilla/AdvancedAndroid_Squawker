a9b9d0786323cd5677911233b7759433315def75
Jantz <jantzillac.93@gmail.com>
2018-05-16 09:53:00 -0500

    Add your new service here, with an intent filter

--- app/src/main/java/android/example/com/squawker/fcm/SquawkFirebaseMessagingService.java
+++ app/src/main/java/android/example/com/squawker/fcm/SquawkFirebaseMessagingService.java
@@ -3,10 +3,12 @@ package android.example.com.squawker.fcm;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.Map;
 
 public class SquawkFirebaseMessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
+        Map<String, String> data = remoteMessage.getData();
     }
 }
