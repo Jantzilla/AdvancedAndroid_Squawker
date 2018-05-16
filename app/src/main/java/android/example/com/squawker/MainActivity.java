c3a681088c081b363eaac67e176c43f53118aec1
Jantz <jantzillac.93@gmail.com>
2018-05-16 09:39:02 -0500

    Override onMessageReceived in SquawkFirebaseMessagingService

--- app/src/main/java/android/example/com/squawker/MainActivity.java
+++ app/src/main/java/android/example/com/squawker/MainActivity.java
@@ -90,7 +90,7 @@ public class MainActivity extends AppCompatActivity implements
 
 
         // COMPLETED (1) Make a new Service in the fcm package that extends from FirebaseMessagingService.
-            // TODO (2) As part of the new Service - Override onMessageReceived. This method will
+            // COMPLETED (2) As part of the new Service - Override onMessageReceived. This method will
             // be triggered whenever a squawk is received. You can get the data from the squawk
             // message using getData(). When you send a test message, this data will include the
             // following key/value pairs:
